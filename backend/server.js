1d1d3eea7e8d509e86f318a3634b65e9edd96bae
shravandoijad <shravandoijad4@gmail.com>
2025-07-02 11:10:24 +0530

    solved vercel4 error

--- backend/server.js
+++ backend/server.js
@@ -16,23 +16,10 @@ const PORT = process.env.PORT || 10000;
 
 
 app.use(express.json());
-const allowedOrigins = [
-  'https://gametrack-git-main-shravans-projects-00476bc1.vercel.app',
-  'https://gametrack-btrez6mk0-shravans-projects-00476bc1.vercel.app',
-  
-  'http://localhost:5173',
-  'http://localhost:5174'
-];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: "*",
+  credentials: true 
 }));
 
 app.use(cookieParser());
