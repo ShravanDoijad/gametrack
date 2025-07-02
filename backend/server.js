6d58ada27eaf57f04b4f8ab399e60623ebd3003d
shravandoijad <shravandoijad4@gmail.com>
2025-07-02 11:23:19 +0530

    solved vercel4 error

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,25 @@ const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 
+
+
+const allowedOrigins = [
+  'https://gametrack-sigma.vercel.app',
+  'http://localhost:5173',
+  'http://localhost:5174'
+];
+
 app.use(cors({
-  origin: "*",
-  credentials: true 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
+
 
 app.use(cookieParser());
 
