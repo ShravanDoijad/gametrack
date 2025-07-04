8f70780d747f240e18ce41cc590bf3f9dc0fac42
shravandoijad <shravandoijad4@gmail.com>
2025-07-04 23:38:25 +0530

    new Integration

--- backend/firebase-admin.js
+++ backend/firebase-admin.js
@@ -2,7 +2,11 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./gametrack-serviceAccount.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 module.exports = admin;
