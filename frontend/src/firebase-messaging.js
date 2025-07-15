c2eae1961d0670e6a8a2c76d93df54373bab586e
shravandoijad <shravandoijad4@gmail.com>
2025-07-15 21:36:17 +0530

    resolve error 2

--- frontend/src/firebase-messaging.js
+++ frontend/src/firebase-messaging.js
@@ -9,7 +9,7 @@ export const requestPermission = async () => {
     const permission = await Notification.requestPermission();
     if (permission === "granted") {
       const token = await getToken(messaging, {
-        vapidKey: process.env.VITE_VAPID_KEY, 
+        vapidKey: import.meta.env.VITE_VAPID_KEY, 
       });
       console.log("✅ FCM Token:", token);
       
